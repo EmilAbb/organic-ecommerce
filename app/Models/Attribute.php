1ce5a52bed4a78f00ff81235bebece44ecbdcbc0
EmilAbb <emilabbasov775@gmail.com>
2023-05-29 00:53:20 +0400

    attributes

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Attribute extends Model
 {
   protected $table = 'attributes';
   protected $guarded = [];
+    public function categories() :BelongsToMany
+    {
+        return $this->belongsToMany(Category::class,AttributeCategory::class,'attribute_id','category_id');
+    }
 }
